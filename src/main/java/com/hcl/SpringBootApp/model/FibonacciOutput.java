dcc1c4ab60383e67b1e45b35f48999452c833848
napstrrox <sanjay_kuma@hcl.com>
2018-04-05 19:47:01 +0530

    Create FibonacciOutput.java

---
+++ src/main/java/com/hcl/SpringBootApp/model/FibonacciOutput.java
@@ -0,0 +1,15 @@
+package com.hcl.SpringBootApp.model;
+
+
+public class FibonacciOutput {
+	
+	private long number;
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+}
