ecb20b3f088c0dc213c69a516b169310a3583e06
napstrrox <sanjay_kuma@hcl.com>
2018-04-06 18:17:11 +0530

    Add files via upload

--- src/main/java/com/hcl/SpringBootApp/service/FibonacciService.java
+++ src/main/java/com/hcl/SpringBootApp/service/FibonacciService.java
@@ -6,22 +6,24 @@ import com.hcl.SpringBootApp.model.FibonacciOutput;
 
 
 @Component
-public class FibonacciService {
+public class FibonacciService{
 	
-	public FibonacciOutput fibonacci(long num) {
+	public FibonacciOutput fibonacci(Object num){
 		FibonacciOutput output = new FibonacciOutput();
 		long a = 0, b = 1, c;
-		
-		if(num <= 0) {
-			throw new IllegalArgumentException ("Wrong input: " + num);
+		if(num instanceof Long) {
+			Long newNum=new Long((long) num);
+			if(num.equals(0)){
+				throw new IllegalArgumentException ("Wrong input: " + newNum);
+			}
+			for(long i = 2; i <= newNum; i++) {
+				c = a+b;
+				a = b;
+				b = c;
+			}
+			output.setNumber(b);
+			return output;
 		}
-		
-		for(long i = 2; i <= num; i++) {
-			c = a+b;
-			a = b;
-			b = c;
-		}
-		output.setNumber(b);
-		return output;
+		throw new NumberFormatException("Wrong input Parameter");
 	}
 }
