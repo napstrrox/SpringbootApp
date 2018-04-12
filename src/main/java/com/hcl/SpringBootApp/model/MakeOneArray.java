8726fe9cf11307bfbd719fb1a23a3542213b434f
napstrrox <sanjay_kuma@hcl.com>
2018-04-12 16:21:17 +0530

    Create MakeOneArray.java

---
+++ src/main/java/com/hcl/SpringBootApp/model/MakeOneArray.java
@@ -0,0 +1,37 @@
+package com.hcl.SpringBootApp.model;
+
+import java.util.Arrays;
+
+public class MakeOneArray {
+	private Integer [] array1;
+	private Integer [] array2;
+	private Integer [] array3;
+	
+	public MakeOneArray() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer[] getArray1() {
+		return array1;
+	}
+
+	public void setArray1(Integer[] array1) {
+		this.array1 = array1;
+	}
+
+	public Integer[] getArray2() {
+		return array2;
+	}
+
+	public void setArray2(Integer[] array2) {
+		this.array2 = array2;
+	}
+
+	public Integer[] getArray3() {
+		return array3;
+	}
+
+	public void setArray3(Integer[] array3) {
+		this.array3 = array3;
+	}
+}
